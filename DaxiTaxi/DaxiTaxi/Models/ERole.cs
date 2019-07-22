e8a918b91e636fe1ca1b0a0c32f4d4765c1a1cec
Mitra Pantic <mitra196506@gmail.com>
2019-07-22 10:46:14 +0200

    Added project and user model

---
+++ DaxiTaxi/DaxiTaxi/Models/ERole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DaxiTaxi.Models
+{
+    public enum ERole
+    {
+        ADMIN,
+        CUSTOMER,
+        DRIVER
+    }
+}
---
+++ DaxiTaxi/DaxiTaxi/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DaxiTaxi.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public EGender Gender { get; set; }
+
+        public string JMBG { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public ERole Role { get; set; }
+
+       // public Ride Ride { get; set; }
+    }
+}
